97cb2a37000cfe7e01de89949832e96a12bf6dd1
aadi2497 <w0751432@myscc.ca>
2022-11-14 22:41:11 -0500

    ISP_Call.cs Created and Updated

---
+++ AdityaBooks.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+
+namespace AdityaBooks.DataAccess.Repository.IRepository
+{
+    public interface ISP_Call : IDisposable
+    {
+        T Single<T>(string procedurename, DynamicParameters param = null); //Need to install Dapper Nuget Package 3.16.xx
+
+        void Execute(string procedurename, DynamicParameters param = null);
+
+        T OneRecord<T>(string procedurename, DynamicParameters param = null);
+
+        IEnumerable<T> List<T>(string procedurename, DynamicParameters param = null);
+
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedurename, DynamicParameters param = null);
+    }
+}
