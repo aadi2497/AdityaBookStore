271f1a56ccee812a3af2057cc5809c42aadd8373
aadi2497 <w0751432@myscc.ca>
2022-11-14 22:19:05 -0500

    New Catergory Repos Created

---
+++ AdityaBooks.DataAccess/Repository/IRepository/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using AdityaBooks.DataAccess.Repository.IRepository;
+using AdityaBooks.Models;
+using AdityaBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace AdityaBooks.DataAccess.Repository.IRepository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
