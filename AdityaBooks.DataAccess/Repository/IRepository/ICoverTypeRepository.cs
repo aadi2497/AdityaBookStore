fc32a2e7968d888ce850934c7c102e7941bb6e9c
aadi2497 <w0751432@myscc.ca>
2022-11-23 19:28:10 -0500

    DataAccess > CoverType Created In Repository Folder

---
+++ AdityaBooks.DataAccess/Repository/IRepository/ICoverTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using AdityaBooks.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdityaBooks.DataAccess.Repository.IRepository
+{
+    public interface ICoverTypeRepository : IRepository<CoverType>
+    {
+        void Update(CoverType covertype);
+    }
+}
