eebbba0eb9ec1b021c81681eb44b77771cd6b99e
aadi2497 <w0751432@myscc.ca>
2022-11-30 21:40:59 -0500

    Update Files accroding to GIthuB link

--- AdityaBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ AdityaBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -12,62 +12,61 @@ namespace AdityaBookStore.Areas.Admin
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+
         public CategoryController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
+
         public IActionResult Index()
         {
             return View();
         }
+
         public IActionResult Upsert(int? id)
         {
             Category category = new Category();
             if (id == null)
             {
+                // this is for create
                 return View(category);
-
             }
+            // this is for edit
             category = _unitOfWork.Category.Get(id.GetValueOrDefault());
             if (category == null)
             {
                 return NotFound();
             }
-            return View();
+            return View(category);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Upsert(Category category)
         {
-            if (ModelState.IsValid)                     //Checks all validation in the model
+            if (ModelState.IsValid)
             {
                 if (category.Id == 0)
-
                 {
                     _unitOfWork.Category.Add(category);
-
                 }
                 else
                 {
                     _unitOfWork.Category.Update(category);
                 }
                 _unitOfWork.Save();
-                return RedirectToAction(nameof(Index));         //To see all the categories
+                return RedirectToAction(nameof(Index));
             }
             return View(category);
         }
 
-
-        //API calls here
         #region API CALLS
-        [HttpGet]
 
+        [HttpGet]
         public IActionResult GetAll()
         {
-            //return NotFound();
-            var allobj = _unitOfWork.Category.GetAll();
-            return Json(new { data = allobj });
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
         }
 
         [HttpDelete]
@@ -77,12 +76,13 @@ namespace AdityaBookStore.Areas.Admin
             if (objFromDb == null)
             {
                 return Json(new { success = false, message = "Error while deleting" });
-
             }
             _unitOfWork.Category.Remove(objFromDb);
             _unitOfWork.Save();
-            return Json(new { success = true, messasge = "Delete successful" });
+            return Json(new { success = true, message = "Delete Successful" });
         }
+
         #endregion
+
     }
 }
--- AdityaBooks.DataAccess/Repository/IRepository/ISP_Call.cs
+++ AdityaBooks.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -7,14 +7,14 @@ namespace AdityaBooks.DataAccess.Repository.IRepository
 {
     public interface ISP_Call : IDisposable
     {
-        T Single<T>(string procedurename, DynamicParameters param = null); //Need to install Dapper Nuget Package 3.16.xx
+        T Single<T>(string procedureName, DynamicParameters param = null);
 
-        void Execute(string procedurename, DynamicParameters param = null);
+        void Execute(string procedureName, DynamicParameters param = null);
 
-        T OneRecord<T>(string procedurename, DynamicParameters param = null);
+        T OneRecord<T>(string procedureName, DynamicParameters param = null);
 
-        IEnumerable<T> List<T>(string procedurename, DynamicParameters param = null);
+        IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null);
 
-        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedurename, DynamicParameters param = null);
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);
     }
 }
